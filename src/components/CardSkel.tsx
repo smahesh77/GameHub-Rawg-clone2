316503c47caff2bcaea4d37a0434a368046107ce
smahesh77 <smahesh7736@gmail.com>
2023-09-12 15:58:35 +0530

    fixed skeletons

--- src/components/CardSkel.tsx
+++ src/components/CardSkel.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CardSkel = () => {
   return (
-    <Card width={'400px'}borderRadius={10} overflow="hidden">
+    <Card width={'400px'} borderRadius={10} overflow="hidden">
       <Skeleton height="300px" />
       <CardBody>
         <SkeletonText></SkeletonText>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,25 +8,29 @@ import { Platform } from "../hooks/usePlatforms";
 import { GameQuery } from "../App";
 
 interface Props {
-  
-  GameObj: GameQuery|null
+  GameObj: GameQuery | null;
 }
 
-const GameGrid = ({ GameObj}: Props) => {
+const GameGrid = ({ GameObj }: Props) => {
   const { data, error, isLoading } = useGames(GameObj);
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6,7,8,9, 10, 11, 12, 13];
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl:4 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         spacing={6}
         padding={"10px"}
       >
         {" "}
         {/** pretty straightforward right */}
-        {isLoading && skeletons.map((skeleton) => <CardSkel key={skeleton}></CardSkel>)}
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <CardStyle>
+              <CardSkel key={skeleton}></CardSkel>
+            </CardStyle>
+          ))}
         {data?.map((game) => (
           <CardStyle key={game.id}>
             <GameCard game={game}></GameCard>
