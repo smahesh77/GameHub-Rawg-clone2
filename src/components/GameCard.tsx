0b9e47bd593ef8cc3e06b3e83bb7345a73b1ab5f
smahesh77 <smahesh7736@gmail.com>
2023-09-15 23:15:47 +0530

    download funtionality

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,14 @@
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  HStack,
+  Heading,
+  Image,
+  Link,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 import { Game } from "../hooks/useGames";
 import PlatFormListIcons from "./PlatFormListIcons";
@@ -10,13 +20,10 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card
-      onClick={() => console.log(`clicked ${game.name}`)}
-      borderBottom={26}
-    >
+    <Card onClick={() => console.log(`clicked ${game.name}`)} borderBottom={26}>
       <Image src={game.background_image}></Image>
       <CardBody border={6}>
-      <HStack justify='space-between' mb={3}>
+        <HStack justify="space-between" mb={3}>
           <PlatFormListIcons
             platforms={game.parent_platforms.map((p) => p.platform)}
           ></PlatFormListIcons>
@@ -24,9 +31,12 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic}></CriticScore>
         </HStack>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <HStack p='4'>
+          <Spacer />
+          <Button colorScheme="green" target="_blank" href={`https://fitgirl-repacks.site/?s=${game.name.replace(/ /g, "+").toLowerCase()}`} as="a" textDecoration="none">Download</Button>
+        </HStack>
         {/* {game.parent_platforms.map((platform) => <Text >{platform.platform.name}</Text>)} 
             you can destructure the object here like({platform}) => <>{platform.name}<> */}
-        
       </CardBody>
     </Card>
   );
